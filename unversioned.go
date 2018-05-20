d5e188d968e3d0caf31f070f02e317864a7eb6d8
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-20 22:11:58 +0700

    Add customizable class name

--- unversioned.go
+++ unversioned.go
@@ -2,17 +2,19 @@ package cundef
 
 // UnversionedSingleAtomicAssetGroup is a description of asset group in which all assets are single atoms and don't need to CVSd
 type UnversionedSingleAtomicAssetGroup struct {
-	Title    string `json:"title"`
-	Root     string `json:"root"`
-	Codepath string `json:"codepath"`
+	Title     string `json:"title"`
+	Root      string `json:"root"`
+	Codepath  string `json:"codepath"`
+	Classname string `json:"classname"`
 }
 
 // UnversionedMultiAtomicAssetGroup is a description of an asset group in which all assets are multi-atomic and don't need to be CVSd
 type UnversionedMultiAtomicAssetGroup struct {
-	Title    string                `json:"title"`
-	Root     string                `json:"root"`
-	Codepath string                `json:"codepath"`
-	Asset    UnversionedMultiAsset `json:"asset"`
+	Title     string                `json:"title"`
+	Root      string                `json:"root"`
+	Codepath  string                `json:"codepath"`
+	Asset     UnversionedMultiAsset `json:"asset"`
+	Classname string                `json:"classname"`
 }
 
 // UnversionedMultiAsset is an asset that has multipathing applied and features a couple of... things
--- versioned.go
+++ versioned.go
@@ -2,17 +2,19 @@ package cundef
 
 // VersionedSingleAtomicAssetGroup is a description of a group of assets that get version tracked, but yet are single atoms
 type VersionedSingleAtomicAssetGroup struct {
-	Title    string `json:"title"`
-	Root     string `json:"root"`
-	Codepath string `json:"codepath"`
+	Title     string `json:"title"`
+	Root      string `json:"root"`
+	Codepath  string `json:"codepath"`
+	Classname string `json:"classname"`
 }
 
 // VersionedMultiAtomicAssetGroup is a description of a group of assets that get version tracked and are complex
 type VersionedMultiAtomicAssetGroup struct {
-	Title    string                    `json:"title"`
-	Root     string                    `json:"root"`
-	Codepath string                    `json:"codepath"`
-	Asset    VersionedMultiAtomicAsset `json:"asset"`
+	Title     string                    `json:"title"`
+	Root      string                    `json:"root"`
+	Codepath  string                    `json:"codepath"`
+	Classname string                    `json:"classname"`
+	Asset     VersionedMultiAtomicAsset `json:"asset"`
 }
 
 // VersionedMultiAtomicAsset is a description of complex object in addition to being versioned and path tracked
