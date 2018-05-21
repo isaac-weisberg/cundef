bad81e90548ea251eff015ec447f308f0e1b72c0
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-05-21 10:51:43 +0700

    Add configuration of class name

--- project.go
+++ project.go
@@ -3,6 +3,7 @@ package cundef
 // Project is the root struct used for serialization
 type Project struct {
 	ProjectTitle string                              `json:"title"`
+	Classname    string                              `json:"classname"`
 	Unsin        []UnversionedSingleAtomicAssetGroup `json:"unsin"`
 	Unmul        []UnversionedMultiAtomicAssetGroup  `json:"unmul"`
 	Versin       []VersionedSingleAtomicAssetGroup   `json:"versin"`
